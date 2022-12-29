41445b1ff580b4eb0c39588f81822ab91541352d
Luong Phat Nguyen <luong.nguyen@insa-cvl.fr>
2022-12-29 08:27:03 +0100

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     siteTitle: `Luong Phat Nguyen`,
     siteTitleAlt: `Luong Phat Nguyen`,
     siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    siteUrl: `lphatnguyen.github.io`,
     siteDescription: `Luong Phat Nguyen's personal website`,
     siteImage: `/banner.jpg`,
     author: `Luong Phat Nguyen`,
